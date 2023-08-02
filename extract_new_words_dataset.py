4e97ac3e18209f4e85dc92019df5d185e645716e
Christian Huber <ukekk@student.kit.edu>
2023-08-02 12:13:17 +0200

    Updated dataset extraction file to make sure there are no overlapping train/test words

--- extract_new_words_dataset.py
+++ extract_new_words_dataset.py
@@ -3,8 +3,6 @@ import sys
 import glob
 import random
 
-random.seed(42)
-
 replacements = {tmp[0]:tmp[1] for line in open("aligned_21/replacements.txt") if line[0] != "#" and (tmp := line.strip().split("|"))}
 filtering = set([line.strip() for line in open("aligned_21/all_new_words_filtered.txt") if line[0] != "#"])
 filtering2 = set(tmp[0].lower() for line in open("training_data.txt") if (tmp := line.strip().split()))
@@ -51,10 +49,22 @@ for word,s in counter_word.items():
         cat = list(word_to_cats[word])[0]
         s[i][1] = cat
 
+segments_to_references = {}
+for x in counter_word.values():
+    for (f,_),_,line_id in x:
+        for i,line in enumerate(open(f[:-len("new_words")]+"ref")):
+            if int(line_id) == i:
+                ref = line.strip()
+                for s,r in replacements.items():
+                    ref = ref.replace(s,r)
+                segments_to_references[(f,line_id)] = ref
+
 counter_word = list(counter_word.items())
 counter_word.sort(key=lambda item: len(item[1])) # first choose segments of words with few occurances
 
-used = set()
+used_segments = set()
+used_new_words = set()
+used_references = set()
 data = [[] for _ in range(6)]
 no2speakers = 0
 for word_id, (word, s) in enumerate(counter_word):
@@ -69,12 +79,24 @@ for word_id, (word, s) in enumerate(counter_word):
     speakers.sort(key=lambda item: len(item[1])) # choose speaker which has spoken the new word the fewest times as test (if possible)
 
     index_set = 0
+    num_added = 0
+    new_references = set()
     for index_speaker, (s_id, line_ids) in enumerate(speakers):
         for line_id in line_ids:
             file = s_id[0]
-            used_key = (file,line_id)
-            if used_key not in used:
-                used.add(used_key)
+            used_segment = (file,line_id)
+
+            # make sure every segment is only used once
+            # make sure a used new word does not occur in samples of other new words
+            if used_segment not in used_segments and \
+                not any(new_word in segments_to_references[used_segment] for new_word in used_new_words) and \
+                not any(word in reference for reference in used_references):
+
+                used_segments.add(used_segment)
+                new_references.add(segments_to_references[used_segment])
+                if index_set == 0:
+                    used_segment_0 = used_segment
+                num_added += 1
 
                 data[index_set].append((file,line_id,word,word_id,cat))
 
@@ -86,23 +108,30 @@ for word_id, (word, s) in enumerate(counter_word):
                         else:
                             break
 
-    if index_set == 0:
+    if num_added == 0:
         pass #print("FOUND NONE")
-    elif index_set == 1:
+    elif num_added == 1:
         data[0] = data[0][:-1]
-        pass #print("FOUND NO 2ND")
+        used_segments.remove(used_segment_0)
+        #print("FOUND NO 2ND")
+    else:
+        used_new_words.add(word)
+        for reference in new_references:
+            used_references.add(reference)
 
 print("No two speakers for",no2speakers,"samples!")
 
-percent_devset = 10
-
 cat_to_indices = {} # maps categories to word indices where the word is of the corresponding category
-for i,s in enumerate(data[0]):
+for s in data[0]:
     cat = s[-1]
+    i = s[3]
     if cat not in cat_to_indices:
         cat_to_indices[cat] = [i]
     else:
         cat_to_indices[cat].append(i)
+
+percent_devset = 15
+random.seed(42)
 
 indices_dev = set(index for cat,indices in cat_to_indices.items() for index in random.sample(indices,int(len(indices)*percent_devset/100))) # randomly choose percent_devset % of each category for dev set
 
@@ -120,5 +149,7 @@ for split in ["dev","test"]: # write out files
                     for i,line in enumerate(open(file[:-len("new_words")]+"ref")):
                         if str(i) == line_index:
                             break
+                    for s,r in replacements.items():
+                        line = line.replace(s,r)
                     f2.write(line)
                     f3.write(new_word+"|"+cat+"\n")
